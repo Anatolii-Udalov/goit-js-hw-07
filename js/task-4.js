594c03f71dd3d62466daa3b7877ccc9ab14d41e4
Anatolii Udalov <anatoly.uudalov@gmail.com>
2024-12-16 14:06:50 +0200

    forth_hw

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const formElement = document.querySelector('.login-form');
+formElement.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = {
+        [formElement.elements.email.name]: formElement.elements.email.value.trim(),
+        [formElement.elements.password.name]: formElement.elements.password.value.trim(),
+    };
 
+    if (formElement.elements.email.value === '' || formElement.elements.password.value === '') {
+        alert('All form fields must be filled in');
+    }
+    else {
+        console.log(formData);
+    };
+    formElement.reset();
+});

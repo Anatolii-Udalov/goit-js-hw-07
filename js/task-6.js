72d2eb8ee2a013a98c15d8e5cff1b6f029162200
Anatolii Udalov <anatoly.uudalov@gmail.com>
2024-12-16 20:45:47 +0200

    css_update

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,21 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const elementMenu = document.querySelector('#controls');
+const inputEl = elementMenu.querySelector('input');
+const btnCreate = elementMenu.querySelector('button');
+const btnDestroy = elementMenu.lastChild;
+
+const randomColor = `#${Math.floor(Math.random() * 16777215)
+  .toString(16)
+  .padStart(6, 0)}`;
+
+
+
+const boxesEl = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', () => {
+  if (0 <= inputEl.value >= 100) {
+    boxesEl.style.width = '30px';
+    boxesEl.style.hight = '30px';
+    boxesEl.style.bacgroundColor = randomColor;
+  }
+});
+

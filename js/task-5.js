6ba548dd2c7dc8b220dc00dc13d85e7d5e3f8bf2
Anatolii Udalov <anatoly.uudalov@gmail.com>
2024-12-16 14:48:05 +0200

    update_hw

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+const btnChngColor = document.querySelector('.change-color');
+const spanChngColor = document.querySelector('.color');
+const bodyColor = document.querySelector("body");
+
+btnChngColor.addEventListener('click', () => {
+  const randomColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+
+  spanChngColor.style.color = randomColor;
+
+  bodyColor.style.backgroundColor = randomColor;
+});

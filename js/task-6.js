5e4dd32d86acba7a017d10008eaefb9e17e02d36
Anatolii Udalov <anatoly.uudalov@gmail.com>
2024-12-16 20:50:09 +0200

    final_update

--- js/task-6.js
+++ js/task-6.js
@@ -1,21 +1,45 @@
 const elementMenu = document.querySelector('#controls');
 const inputEl = elementMenu.querySelector('input');
-const btnCreate = elementMenu.querySelector('button');
-const btnDestroy = elementMenu.lastChild;
-
-const randomColor = `#${Math.floor(Math.random() * 16777215)
-  .toString(16)
-  .padStart(6, 0)}`;
-
-
-
+const btnCreate = elementMenu.querySelector('[data-create]');
+const btnDestroy = elementMenu.querySelector('[data-destroy]');
 const boxesEl = document.querySelector('#boxes');
 
-btnCreate.addEventListener('click', () => {
-  if (0 <= inputEl.value >= 100) {
-    boxesEl.style.width = '30px';
-    boxesEl.style.hight = '30px';
-    boxesEl.style.bacgroundColor = randomColor;
+const getRandomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+
+
+const createBoxes = (amount) => {
+
+  boxesEl.innerHTML = '';
+
+  let size = 30;
+  const boxFragments = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomColor();
+    box.style.margin = '5px';
+    size += 10;
+    boxFragments.appendChild(box);
   }
+
+  boxesEl.appendChild(boxFragments);
+};
+
+btnCreate.addEventListener('click', () => {
+  const amount = Number(inputEl.value);
+
+  if (amount < 1 || amount > 100) {
+    alert('Введіть значення від 1 до 100.');
+    inputEl.value = '';
+    return;
+  }
+
+  createBoxes(amount);
+  inputEl.value = '';
 });
 
+btnDestroy.addEventListener('click', () => {
+  boxesEl.innerHTML = '';
+});
